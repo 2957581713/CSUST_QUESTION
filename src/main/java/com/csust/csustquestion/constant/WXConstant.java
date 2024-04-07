ac95ebe16c15c8af632ef66e1378f8eb6db3c6bf
luohaibin <2957581713@qq.com>
2024-04-08 01:19:56 +0800

    8.1 微信小程序接口开发完成，初步重构完成

--- src/main/java/com/csust/csustquestion/constant/WXConstant.java
+++ src/main/java/com/csust/csustquestion/constant/WXConstant.java
@@ -3,5 +3,5 @@ public class WXConstant {
 //  public static final String APPID = "wx900e15fea921fe3c";
   public static final String APPID = "wxd17a5360450240b3";
 
-  public static final String SECRET = "246e03792bf989c624de572ae873605e";
+  public static final String SECRET = "";
 }
